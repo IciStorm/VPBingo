f61750c13f83ce109c14d3427a3b8454550d86b0
Ici <97318691+IciStorm@users.noreply.github.com>
2024-09-01 17:49:34 -0700

    Validation Set up

---
+++ validCombos.js
@@ -0,0 +1,8 @@
+const validCombinations = {
+    'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Arocknid.PNG?raw=true': [
+        'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_VisitRibbon.PNG?raw=true',
+        'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_VariantRibbon.PNG?raw=true'
+    ],
+    'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Badgesicie.PNG?raw=true': [
+        'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_RomanceRibbon.PNG?raw=true'
+    ],
