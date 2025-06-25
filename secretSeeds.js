e35a1d50d3f28dbb235f0a3701848737a2da32fa
Ici <97318691+IciStorm@users.noreply.github.com>
2025-06-24 20:51:46 -0700

    flutterscotch secret seed

--- secretSeeds.js
+++ secretSeeds.js
@@ -1,9 +1,15 @@
 const secretSeeds = {
   "flutterscotch": {
     iconSet: [
-      "img1",
-      "img2",
-      "img3"
+      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Black.PNG?raw=true',
+      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Blue.PNG?raw=true',
+      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Brown.PNG?raw=true',
+      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Green.PNG?raw=true',
+      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Orange.PNG?raw=true',
+      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Pink.PNG?raw=true',
+      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Purple.PNG?raw=true',
+      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Red.PNG?raw=true',
+      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_White.PNG?raw=true'
     ]
   },
 };
