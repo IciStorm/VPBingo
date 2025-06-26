b00107cda264fb983d701764c8118c5d6d674cd8
Ici <97318691+IciStorm@users.noreply.github.com>
2025-06-26 15:03:21 -0700

    convert secretseeds to object

--- secretSeeds.js
+++ secretSeeds.js
@@ -1,15 +1,15 @@
 const secretSeeds = {
   "flutterscotch": {
     iconSet: [
-      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Black.PNG?raw=true',
-      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Blue.PNG?raw=true',
-      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Brown.PNG?raw=true',
-      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Green.PNG?raw=true',
-      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Orange.PNG?raw=true',
-      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Pink.PNG?raw=true',
-      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Purple.PNG?raw=true',
-      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Red.PNG?raw=true',
-      'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_White.PNG?raw=true'
+      { name: 'Flutterscotch_Black', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Black.PNG?raw=true', level: 3 },
+      { name: 'Flutterscotch_Blue', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Blue.PNG?raw=true', level: 3 },
+      { name: 'Flutterscotch_Brown', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Brown.PNG?raw=true', level: 3 },
+      { name: 'Flutterscotch_Green', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Green.PNG?raw=true', level: 3 },
+      { name: 'Flutterscotch_Orange', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Orange.PNG?raw=true', level: 3 },
+      { name: 'Flutterscotch_Pink', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Pink.PNG?raw=true', level: 3 },
+      { name: 'Flutterscotch_Purple', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Purple.PNG?raw=true', level: 3 },
+      { name: 'Flutterscotch_Red', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Red.PNG?raw=true', level: 3 },
+      { name: 'Flutterscotch_White', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_White.PNG?raw=true', level: 3 }
     ]
   },
 };
