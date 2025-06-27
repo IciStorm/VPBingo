6eea658d44bdc776f8414695acfee6c2d6f17571
Ici <97318691+IciStorm@users.noreply.github.com>
2025-06-26 17:17:26 -0700

    Add Yellow Flutterscotch

--- pinataIcons.js
+++ pinataIcons.js
@@ -27,7 +27,7 @@ const pinataIcons = [
   { name: 'Flutterscotch_Purple', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Purple.PNG?raw=true', level: 2 },
   { name: 'Flutterscotch_Red', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Red.PNG?raw=true', level: 2 },
   { name: 'Flutterscotch_White', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_White.PNG?raw=true', level: 3 },
-  { name: 'Flutterscotch_Yellow', url: '', level: 1 },
+  { name: 'Flutterscotch_Yellow', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Yellow.PNG?raw=true', level: 1 },
   { name: 'Fourheads', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Fourheads.PNG?raw=true', level: 8 },
   { name: 'Fudgehog', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Fudgehog.PNG?raw=true', level: 4 },
   { name: 'Galagoogoo', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Galagoogoo.PNG?raw=true', level: 9 },
