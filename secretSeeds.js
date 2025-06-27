d28e9065649c2c81542595ec97d0c2023ee72975
Ici <97318691+IciStorm@users.noreply.github.com>
2025-06-26 21:53:43 -0700

    Update secretSeeds.js

--- secretSeeds.js
+++ secretSeeds.js
@@ -9,7 +9,8 @@ const secretSeeds = {
       { name: 'Flutterscotch_Pink', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Pink.PNG?raw=true', level: 3 },
       { name: 'Flutterscotch_Purple', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Purple.PNG?raw=true', level: 3 },
       { name: 'Flutterscotch_Red', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Red.PNG?raw=true', level: 3 },
-      { name: 'Flutterscotch_White', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_White.PNG?raw=true', level: 3 }
+      { name: 'Flutterscotch_White', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_White.PNG?raw=true', level: 3 },
+      { name: 'Flutterscotch_Yellow', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Yellow.PNG?raw=true', level: 1 }
     ]
   },
 };
