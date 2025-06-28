0486542c1228c302024a36588cfdbf28d6a54128
Ici <97318691+IciStorm@users.noreply.github.com>
2025-06-27 21:54:52 -0700

    Update secretSeeds.js

--- secretSeeds.js
+++ secretSeeds.js
@@ -13,4 +13,55 @@ const secretSeeds = {
       { name: 'Flutterscotch_Yellow', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/UI_Icon_Pinata_Flutterscotch_Yellow.PNG?raw=true', level: 1 }
     ]
   },
+  "seedsonly": {
+    iconSet: [
+      { name: 'Apple', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Apple.PNG?raw=true', level: 4 },
+      { name: 'Banana', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Banana.PNG?raw=true', level: 7 },
+      { name: 'BirdOfParadise', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/BirdOfParadise.PNG?raw=true', level: 8 },
+      { name: 'Blackberry', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Blackberry.PNG?raw=true', level: 4 },
+      { name: 'Bluebell', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Bluebell.PNG?raw=true', level: 2 },
+      { name: 'Bullrush', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Bullrush.PNG?raw=true', level: 7 },
+      { name: 'Buttercup', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Buttercup.PNG?raw=true', level: 1 },
+      { name: 'Carrot', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Carrot.PNG?raw=true', level: 1 },
+      { name: 'Chili', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Chili.PNG?raw=true', level: 4 },
+      { name: 'Corn', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Corn.PNG?raw=true', level: 3 },
+      { name: 'Daisy', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Daisy.PNG?raw=true', level: 1 },
+      { name: 'Fir', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Fir.PNG?raw=true', level: 5 },
+      { name: 'Gem', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Gem.PNG?raw=true', level: 9 },
+      { name: 'Gooseberry', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Gooseberry.PNG?raw=true', level: 5 },
+      { name: 'Hazelnut', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Hazelnut.PNG?raw=true', level: 5 },
+      { name: 'Monkeynut', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Monkeynut.PNG?raw=true', level: 6 },
+      { name: 'Nightshade', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Nightshade.PNG?raw=true', level: 7 },
+      { name: 'Oak', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Oak.PNG?raw=true', level: 10 },
+      { name: 'Orchid', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Orchid.PNG?raw=true', level: 9 },
+      { name: 'PoisonIvy', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/PoisonIvy.PNG?raw=true', level: 8 },
+      { name: 'Poppy', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Poppy.PNG?raw=true', level: 2 },
+      { name: 'Pumpkin', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Pumpkin.PNG?raw=true', level: 3 },
+      { name: 'Snapdragon', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Snapdragon.PNG?raw=true', level: 6 },
+      { name: 'Sunflower', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Sunflower.PNG?raw=true', level: 4 },
+      { name: 'Thistle', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Thistle.PNG?raw=true', level: 3 },
+      { name: 'Toadstool', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Toadstool.PNG?raw=true', level: 6 },
+      { name: 'Tulip', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Tulip.PNG?raw=true', level: 5 },
+      { name: 'Turnip', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Turnip.PNG?raw=true', level: 1 },
+      { name: 'VenusPinataTrap', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/VenusPinataTrap.PNG?raw=true', level: 8 },
+      { name: 'WaterLily', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/WaterLily.PNG?raw=true', level: 5 },
+      { name: 'Watercress', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Plants/Watercress.PNG?raw=true', level: 3 }    
+    ]
+  },
+  "tinkeronly": {
+    iconSet: [
+      { name: 'Apple', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Apple.PNG?raw=true', level: 4 },
+      { name: 'Banana', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Banana.PNG?raw=true', level: 7 },
+      { name: 'Blackberry', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Blackberry.PNG?raw=true', level: 4 },
+      { name: 'Bread', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Bread.PNG?raw=true', level: 3 },
+      { name: 'Carrot', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Carrot.PNG?raw=true', level: 1 },
+      { name: 'Corn', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Corn.PNG?raw=true', level: 2 },
+      { name: 'Gem', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Gem.PNG?raw=true', level: 9 },
+      { name: 'Gooseberry', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Gooseberry.PNG?raw=true', level: 5 },
+      { name: 'Honey', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Honey.PNG?raw=true', level: 4 },
+      { name: 'Milk', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Milk.PNG?raw=true', level: 7 },
+      { name: 'Monkeynut', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Monkeynut.PNG?raw=true', level: 7 },
+      { name: 'Pumpkin', url: 'https://github.com/IciStorm/VPBingo/blob/main/images/Tinkerables/Pumpkin.PNG?raw=true', level: 3 }
+    ]
+  },
 };
