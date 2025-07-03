364c9cfee0c63470772f2d1a87d03dc7af463161
Ici <97318691+IciStorm@users.noreply.github.com>
2025-07-03 12:41:31 -0700

    Create awardTypes.js

---
+++ awardTypes.js
@@ -0,0 +1,16 @@
+const awardTypeIcons = [
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_ErrorHandler.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_Attract.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_VisitRibbon.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_ResideRibbon.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_VariantRibbon1.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_VariantRibbon2.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_VariantRibbon3.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_RomanceRibbon.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_MasterRomanceRibbon.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_PlantRibbon.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_Packet_Fertilizer_Magic.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_Character_Bart.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_Check.PNG?raw=true',
+  'https://github.com/IciStorm/VPBingo/blob/main/images/awards/UI_Icon_Coin.PNG?raw=true'
+];
